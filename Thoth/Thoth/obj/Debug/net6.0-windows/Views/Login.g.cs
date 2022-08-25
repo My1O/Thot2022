f2bf0430c07a4485a39bbbb34a775cb7fd2e2630
Camilo Castro <castroh24@gmail.com>
2022-08-24 22:04:44 -0600

    git ignore incluido

--- Thoth/Thoth/obj/Debug/net6.0-windows/Views/Login.g.cs
+++ Thoth/Thoth/obj/Debug/net6.0-windows/Views/Login.g.cs
@@ -52,7 +52,7 @@ namespace Thoth.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Thoth;V1.0.0.0;component/views/login.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Thoth;component/views/login.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\Login.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
